0ea7178547c1902a2064f268edab6af91e60652a
black-pigeon <1530604142@qq.com>
2022-10-19 13:50:15 +0800

    edit the scripts for general antsdr devices

--- firmware/fpga/antsdr_e200/ip/axi64_8k_2clk_fifo/axi64_8k_2clk_fifo_stub.vhdl
+++ firmware/fpga/antsdr_e200/ip/axi64_8k_2clk_fifo/axi64_8k_2clk_fifo_stub.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
--- Date        : Thu Sep 22 10:05:46 2022
+-- Date        : Wed Oct 19 11:29:55 2022
 -- Host        : mp-dev running 64-bit Kylin V10 SP1
 -- Command     : write_vhdl -force -mode synth_stub
---               /home/mp/wcc_demo/e200_net/ip/axi64_8k_2clk_fifo/axi64_8k_2clk_fifo_stub.vhdl
+--               /home/mp/wcc_demo/git_antsdr_uhd/antsdr_uhd_project/firmware/fpga/antsdr_e200/ip/axi64_8k_2clk_fifo/axi64_8k_2clk_fifo_stub.vhdl
 -- Design      : axi64_8k_2clk_fifo
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7z020clg400-2
