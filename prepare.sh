4401c8fdb51b2540d63c8396775d3bdcaf5df427
info@revskills.de <info@revskills.de>
2023-02-23 20:08:52 +0100

    add helper scripts

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+export CROSS_COMPILE=arm-linux-gnueabihf- 
+#export PATH=$PATH:/opt/Xilinx/Vitis/2021.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin 
+export PATH=$PATH:/opt/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin
+export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2021.1/settings64.sh
+export PERL_MM_OPT=
+export LD_LIBRARY_PATH=
+#export ADI_IGNORE_VERSION_CHECK=1
